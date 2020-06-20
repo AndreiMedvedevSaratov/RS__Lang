94e12d276501f78e0ff95f29ee8a835123c3f31e
Andrei Medvedev <andreiok@gmail.com>
2020-06-20 10:12:17 +0400

    init: start development of About Team page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,32 @@
-<template lang='pug'>
-  div( class="about" )
-    h1 This is an about page
+<template>
+  <div class="about-wrapper">
+    <div class="about">
+      <h1>About "Dream Team 42"</h1>
+    </div>
+    <div class="p1">
+      <p>Here is short information about our team. First question is "Why Dream Team 42?</p>
+      <p>Answer is simple ))) - "Dream Team" - just because we are Dream Team ))))</p>
+      <p>"42" - is just because we are 42 team in the main table</p>
+      <p>So let me introduce participants:</p>
+    </div>
+  </div>
 </template>
+
+<script>
+// data:
+// Andrei Medvedev - https://github.com/andreimedvedevsaratov
+// Maksim Kapustin - https://github.com/timeofprogramming
+// Nurlan Kurbandurdyev - https://github.com/ggnk
+// Alexey Zhumaev - https://github.com/alexey221284
+// Pavel Pronchatov - https://github.com/pavelponchatov
+// Mark Flerko - https://github.com/markflerko
+// Aliaksei Palanevich - https://github.com/goldoragon
+
+</script>
+
+<style scoped>
+	.about-wrapper {
+		max-width: 1440px;
+		margin: 0 auto;
+	}
+</style>
