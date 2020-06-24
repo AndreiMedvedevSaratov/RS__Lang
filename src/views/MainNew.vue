024f7bbd8a59bfb900b0b412bc178346afd3cc71
GGnk <Super-nurlan94@yandex.ru>
2020-06-24 13:20:18 +0400

    fix: run version 0.1.0

--- src/views/MainNew.vue
+++ src/views/MainNew.vue
@@ -6,76 +6,67 @@
 			app
 		)
 			v-list( dense)
-			template( v-for="item in items")
-				v-row(
-					v-if="item.heading"
-					:key="item.heading"
-					align="center"
-				)
-				v-col( cols="6")
-					v-subheader( v-if="item.heading") {{ item.heading }}
+				template( v-for="item in items")
+					v-row(
+						v-if="item.heading"
+						:key="item.heading"
+						align="center"
+					)
+						v-col( cols="6")
+							v-subheader( v-if="item.heading") {{ item.heading }}
 
-				v-col(
-					cols="6"
-					class="text-center"
-				)
-					a(
-					href="#!"
-					class="body-2 black--text") EDIT
+						v-col(
+							cols="6"
+							class="text-center"
+						)
+							a(
+							href="#!"
+							class="body-2 black--text") EDIT
 
-				v-list-group(
-					v-else-if="item.children"
-					:key="item.text"
-					v-model="item.model"
-					:prepend-icon="item.model ? item.icon : item['icon-alt']"
-					append-icon=""
-				)
-				template( v-slot:activator)
-					v-list-item-content
-						v-list-item-title {{ item.text }}
+					v-list-group(
+						v-else-if="item.children"
+						:key="item.text"
+						v-model="item.model"
+						:prepend-icon="item.model ? item.icon : item['icon-alt']"
+						append-icon=""
+					)
+						template( v-slot:activator)
+							v-list-item-content
+								v-list-item-title {{ item.text }}
 
-				v-list-item(
-					v-for="(child, i) in item.children"
-					:key="i"
-					link
-				)
-					v-list-item-action( v-if="child.icon")
-					v-icon {{ child.icon }}
-					v-list-item-content
-						v-list-item-title {{ child.text }}
+						v-list-item(
+							v-for="(child, i) in item.children"
+							:key="i"
+							link
+						)
+							v-list-item-action( v-if="child.icon")
+							v-icon {{ child.icon }}
+							v-list-item-content
+								v-list-item-title {{ child.text }}
 
-				v-list-item(
-					v-else
-					:key="item.text"
-					link
-				)
-				v-list-item-action
-					v-icon {{ item.icon }}
+					v-list-item(
+						v-else
+						:key="item.text"
+						link
+					)
+						v-list-item-action
+							v-icon {{ item.icon }}
 
-				v-list-item-content
-					v-list-item-title {{ item.text }}
+						v-list-item-content
+							v-list-item-title {{ item.text }}
 
 		v-app-bar(
 			:clipped-left="$vuetify.breakpoint.lgAndUp"
 			app
-			color="blue darken-3"
-			dark
+			color="grey lighten-5"
 		)
 			v-app-bar-nav-icon( @click.stop="drawer = !drawer" )
 			v-toolbar-title(
 				style="width: 300px"
 				class="ml-0 pl-4"
 			)
-				span( class="hidden-sm-and-down") Google Contacts
+				span( class="hidden-sm-and-down") RS Lang
 
-			v-text-field(
-				flat
-				solo-inverted
-				hide-details
-				prepend-inner-icon="mdi-magnify"
-				label="Search"
-				class="hidden-sm-and-down"
-			)
 			v-spacer
 			v-btn( icon)
 				v-icon mdi-apps
