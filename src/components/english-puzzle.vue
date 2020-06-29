0072ec5247cb820f633b5dace6ead2f7f317796d
Alexey Zhumaev <alexey221284@mail.ru>
2020-06-29 20:54:01 +0400

    fix: add header elements & style

--- src/components/english-puzzle.vue
+++ src/components/english-puzzle.vue
@@ -1,5 +1,19 @@
 <template lang='pug'>
 	div
+		.select_wrapper
+			label.level Level:
+				select#selectbox1(
+					ref="selectbox1"
+					)
+			label.page Page:
+				select#selectbox2(
+					ref="selectbox2"
+				)
+		.button_wrapper
+			button#button_1
+			button#button_2
+			button#button_3
+			button#button_4
 		.result-word(
 			ref="resultWord"
 		)
@@ -12,20 +26,21 @@
 			ref="beginWord"
 		)
 		.fuck
-			.fuck2 f
+			.fuck2
 		.button
 			div(
 				v-if="gameStatus"
 			)
-				button(
-					class="button__not-know.button_style"
+				button.button__not-know.button_style(
 					@click="dontKnow"
 				) I don't know
 				button.button__check.button_style Check
-			button.button__check.button_style(
+				button.button__check.button_style(
 				v-if="!gameStatus"
 			) Next level
-
+		.card
+			p#textExampleTranslate подсказка-перевод
+			p#audioExample путь к аудио файлу
 </template>
 
 <script>
@@ -69,7 +84,6 @@ export default {
 			const widthPx = (str) => +str.match(/[0-9]/g).join('');
 			const sortArr = (arr) => arr.sort(() => Math.random() - 0.5);
 
-			console.log(this.$refs);
 			const { beginWord, wordContainer } = this.$refs;
 
 			const str = this.words[this.num].textExample;
@@ -311,9 +325,16 @@ url('../assets/fonts/montserrat-v14-latin_cyrillic-regular.woff2') format('woff2
   }
 
 body {
-    font-family: 'Montserrat';
-    font-style: normal;
-    color: black;
+	font-family: 'Montserrat';
+	font-style: normal;
+	color: #476622;
+}
+
+.img {
+	background: url('../assets/img/nature.jpg') no-repeat;
+	background-size: cover;
+	width: 100%;
+	height: 100vh;
 }
 
 .title {
@@ -321,172 +342,238 @@ body {
 	font-size: 4rem;
 	font-weight: 700;
 	display: flex;
-    justify-content: center;
+	justify-content: center;
 }
 
 .description {
 	font-weight: 400;
 	padding-top: 20px;
 	display: flex;
-    justify-content: center;
+	justify-content: center;
 }
 
 form {
 	display: flex;
-    justify-content: center;
-    margin-top: 10px;
+	justify-content: center;
+	margin-top: 10px;
 }
 
 .form-wrapper {
 	width: 369px;
-    display: flex;
-    flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 
 .input-email {
 	display: flex;
-    align-items: center;
-    justify-content: space-between;
+	align-items: center;
+	justify-content: space-between;
 }
 
 .input-password {
 	margin-top: 11px;
 	display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-label {
-	cursor: pointer;
+	justify-content: space-between;
+	align-items: center;
 }
 
 input {
 	width: 220px;
-    height: 30px;
-    border: 1px solid #89C3D9;
-    border-radius: 5px;
+	height: 30px;
+	border: 1px solid #476622;
+	border-radius: 5px;
 }
 
 .button {
 	display: flex;
-    justify-content: center;
-    margin-top: 12px;
+	justify-content: center;
+	margin-top: 12px;
 }
 
 .btn {
 	width: 90px;
-    height: 27px;
-    margin-right: 19px;
-    border-radius: 3px;
-    outline: none;
-    background: burlywood;
-	border: 1px solid #F1DE9C;
+	height: 27px;
+	margin-right: 19px;
+	border-radius: 3px;
+	outline: none;
+	background: #a8d06f;
+	border: 1px solid #a8d06f;
 	cursor: pointer;
 }
 
 .btn__register:hover {
-	color: #fff
+	color: #a8d06f;
 }
 
 .btn__sign-in:hover {
-	color: #fff
+	color: #a8d06f;
 }
 
 .start {
-    display: flex;
-    justify-content: center;
-    margin-top: 20px;
+	display: flex;
+	justify-content: center;
+	margin-top: 20px;
 }
 
 .info {
-    display: flex;
-    justify-content: center;
-    height: 20px;
-    margin-top: 10px;
+	display: flex;
+	justify-content: center;
+	height: 20px;
+	margin-top: 10px;
 }
 
 .result-word {
-    width: 900px;
-    height: 450px;
-    background: aqua;
-    margin: 0 auto;
-    margin-top: 20px;
-
+	width: 900px;
+	height: 450px;
+	background: #476622;
+	margin: 0 auto;
+	margin-top: 20px;
+	border-top: 1px solid;
 }
 
 .begin-word {
-    display: flex;
-    justify-content: center;
-    width: 900px;
-    height: auto;
-    margin: 10px auto 0px auto;
+	display: flex;
+	justify-content: center;
+	width: 900px;
+	height: auto;
+	margin: 10px auto 0px auto;
 }
 
 .word-container {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    width: 100%;
-    height: 45px;
-    background: brown;
-    border: 1px solid;
-    align-items: center;
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	height: 45px;
+	background: #d7e5d2;
+	border: 1px solid;
+	align-items: center;
 }
 
 .gig {
-    flex-grow: 1;
-    width: auto;
-    min-width: 40px;
-    height: 45px;
-    background: burlywood;
-    border: none;
-    display: flex;
-    justify-content: center;
+	flex-grow: 1;
+	width: auto;
+	min-width: 40px;
+	height: 45px;
+	background: #a8d06f;
+	border: none;
+	display: flex;
+	justify-content: center;
+	color: #d7e5d2;
+	font-weight: bold;
 }
 
 .word {
-    width: -webkit-fill-available;
-    height: 45px;
-    border: 1px solid;
-    text-align: center;
-    padding: 12px 5px 12px 5px;
-    background: url('../assets/img/girl.jpg') no-repeat;
-    cursor: pointer
+	width: -webkit-fill-available;
+	height: 45px;
+	border: 1px solid;
+	text-align: center;
+	padding: 12px 5px 12px 5px;
+	background: url('../assets/img/girl.jpg') no-repeat;
+	cursor: pointer;
 }
 
-.fuck {
-    display: flex;
-    width: 600px;
-    font-size: 2px;
-    background: cadetblue;
-    margin: 0 auto;
-    opacity: 1;
-}
+/*.fuck {*/
+/*    display: flex;*/
+/*    width: 900px;*/
+/*    font-size: 2px;*/
+/*    background: #476622;*/
+/*    margin: 0 auto;*/
+/*    opacity: 1;*/
+/*}*/
 
 .button_style {
-    padding: 10px 30px 10px 30px;
-    cursor: pointer;
-    border: 1px solid;
-    border-radius: 5px;
+	padding: 10px 30px 10px 30px;
+	cursor: pointer;
+	border: 1px solid;
+	border-radius: 5px;
+	background: #476622;
+	color: #d7e5d2;
+	font-family: 'Montserrat';
+	width: 180px;
+	height: 50px;
+	position: relative;
+	top: 5px;
 }
 
 .button__check {
-    margin-left: 10px
+	margin-left: 10px
 }
 
 .right {
-    box-shadow: 0 0 5px green;
+	box-shadow: 0 0 5px #476622;
 }
 
 .wrong {
-    box-shadow: 0 0 5px red;
+	box-shadow: 0 0 5px red;
 }
 
 .hide {
-    display: none;
+	display: none;
 }
 
 .hovered {
-    background-color: grey;
+	background-color: #a8d06f;
+}
+
+/*мой код*/
+
+.button_wrapper {
+	position: relative;
+	left: 70vw;
+	top: 1vh;
+}
+
+#button_1, #button_2, #button_3, #button_4 {
+	width: 23px;
+	height: 23px;
+	margin-left: .3vw;
+	color: #476622;
+	border-radius: 5px;
+	border: solid 1px #476622;
+}
+
+#selectbox1, #selectbox2 {
+	width: 75px;
+	height: 25px;
+	margin-left: .3vw;
+	color: #476622;
+	border-radius: 5px;
+	border: solid 1px #476622;
+	font-family: 'Montserrat';
+	font-size: 14px;
+}
+
+.select_wrapper {
+	position: relative;
+	left: 20vw;
+	top: 4vh;
+}
+
+#button_1 {
+	background: url("../assets/img/1.png") no-repeat;
+}
+#button_2 {
+	background: url("../assets/img/2.png") no-repeat;
+}
+
+#button_3 {
+	background: url("../assets/img/3.png") no-repeat;
+}
+
+#button_4 {
+	background: url("../assets/img/4.png") no-repeat;
+}
+
+.card {
+	position: absolute;
+	left: 45vw;
+	top: 6vh;
+	width: auto;
+	height: auto;
+	color:#476622;
+	font-size: 14px;
+	border: solid 1px #476622;
+	border-radius: 5px;
+	font-family: 'Montserrat';
 }
 </style>
