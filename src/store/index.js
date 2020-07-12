1726b6fd9fd4cc1ad819588ab566f6fbede12ac6
GGnk <Super-nurlan94@yandex.ru>
2020-07-12 18:47:25 +0400

    fix: update 5 SpeakIt

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex';
 import app from './app';
 import auth from './auth';
 import user from './user';
-import speakit from './speakit';
 import speaking from './speaking';
 import audiovizov from './audiovizov';
 
@@ -16,7 +15,6 @@ export default new Vuex.Store({
 		auth,
 		app,
 		user,
-		speakit,
 		speaking,
 		audiovizov,
 	},
