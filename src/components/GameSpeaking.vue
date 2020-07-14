c6cb31ced9883da0c071696fda3b5e013c97b73b
Alexey Zhumaev <alexey221284@mail.ru>
2020-07-15 03:21:56 +0400

    refactor style & loading stripe

--- src/components/GameSpeaking.vue
+++ src/components/GameSpeaking.vue
@@ -1,30 +1,34 @@
 <template lang="pug">
 	div( class="game" )
-
 		div( class="main" )
 			video(
-				class="main__image"
+				class="video"
 				controls
 				id="video"
-			)
+				)
 				source(
 					:src="myVideosForGame[step]"
 					type="video/mp4"
-				)
-
+					)
 			div(
-				class="amber darken-4 answer"
-				)
-
+				class="answer"
+			)
 			input(
 				type="button"
-				value="Button"
+				value="Speaking"
 				@click="video()"
 				id="btn"
-			)
-
-			p( class='speech' )
-
+				)
+			p(
+				class='speech'
+				)
+			div( v-if="loading"
+				class="load" ) Loading... please wait...
+				v-progress-linear(
+					indeterminate
+					rounded
+					color="blue darken-2"
+					)
 </template>
 
 <script>
@@ -41,6 +45,7 @@ export default {
 	data: () => ({
 		myVideosForGame: ['./assets/video/first.mp4', './assets/video/second.mp4'],
 		step: 0,
+		loading: false,
 	}),
 	computed: {
 		...mapGetters({
@@ -56,9 +61,29 @@ export default {
 		},
 	},
 	created() {},
-	mounted() {},
+	mounted() {
+		this.loading = true;
+		setTimeout(() => {
+			this.loading = false;
+		}, 2500);
+		this.appHtml([
+			{ one: 'main', key: 'drawer', value: false },
+			{ one: 'main', key: 'breadcrumbs', value: false },
+			{ one: 'app', key: 'background', value: 'blue darken-2' },
+			{ one: 'app', key: 'colorWhite', value: true },
+		]);
+	},
+	beforeDestroy() {
+		this.appHtml([
+			{ one: 'main', key: 'drawer', value: true },
+			{ one: 'main', key: 'breadcrumbs', value: true },
+			{ one: 'app', key: 'background', value: 'grey lighten-5' },
+			{ one: 'app', key: 'colorWhite', value: false },
+		]);
+	},
 	methods: {
 		...mapMutations({
+			appHtml: 'EDIT_HTML',
 			video: 'speaking/SPEAKING_VIDEO',
 			answer: 'speaking/SPEAKING_ANSWER',
 			speak: 'speaking/SPEAKING_SPEAK',
@@ -72,29 +97,54 @@ export default {
 
 <style lang="scss" scoped>
 .game {
-	height: 100%;
-	padding: 0 15%;
-	background-color: #ccd8cc;
+	height: 100vh;
 	box-sizing: border-box;
+}
+p {
+	margin: 0;
+}
+.main {
+	position: relative;
+	width: 100%;
+	height: 450px;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+}
+.video {
+	position: relative;
+	width: 300px;
+	height: 300px;
+	display: flex;
+	margin: 0 auto;
+	align-items: center;
+	border-radius: 5px;
+}
 
-	p {
-		margin: 0;
-	}
+.answer {
+	position: relative;
+	display: block;
+	top: 10px;
+	color: #1976D2;
+	text-align: center;
+}
 
-	.main {
-		height: 80%;
-
-		&__image {
-			padding: 2% 0;
-			max-width: 300px;
-			display: block;
-			margin: 0 auto;
-			align-items: center;
-		}
-	}
-
-	.answer {
-		display: block;
-	}
+#btn {
+	display: block;
+	width: 150px;
+	height: 60px;
+	top: 20px;
+	border-radius: 5px;
+	color: white;
+	align-items: center;
+	background: #1976D2;
+	position: relative;
+	left: 50%;
+	transform: translate(-50%, 0);
+}
+.load {
+	position: relative;
+	top: 20px;
+	color: #1976D2;
 }
 </style>
